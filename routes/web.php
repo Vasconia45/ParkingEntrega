19565fc42237e1f987a3fcefbf52ee37c41807ab
Pablodw <ptorresca21dw@ikzubirimanteo.com>
2022-11-15 13:27:46 +0100

    probando agrupamiento de rutas

--- routes/web.php
+++ routes/web.php
@@ -16,31 +16,33 @@ use App\Http\Controllers\UsuarioController;
 */
 
 Route::view('/', "index")->name('car.show');
-//Route to add a new car into the database
-Route::post('/car', [CarController::class, 'add'])->name('car.add');
-//Route to the delete a car from the database
-Route::delete('/car/{id}', [CarController::class, 'delete'])->name('car.delete');
-//Route to update a car
-Route::put('/car/{id}', [CarController::class, 'edit'])->name('car.edit');
-Route::get('/car/{id}', [CarController::class, 'showEdit'])->name('editcar');
-
-
-
 //Routes for the yield/section part
 Route::view('/car', 'layouts.newcars')->name('addcar');
-Route::get('/list', [CarController::class, 'showListCars'])->name('listcar');
 Route::view('/search', 'layouts.searchcars')->name('searchcar');
 
-//Route for searching cars
-Route::post('/search', [CarController::class, 'check'])->name('car.search');
+Route::controller(CarController::class)->group(function(){
+    //Route to add a new car into the database
+    Route::post('/car', 'add')->name('car.add');
+    //Route to the delete a car from the database
+    Route::delete('/car/{id}','delete')->name('car.delete');
+    //Route to update a car
+    Route::put('/car/{id}', 'edit')->name('car.edit');
+    Route::get('/car/{id}', 'showEdit')->name('editcar');
+    Route::get('/list', 'showListCars')->name('listcar');
+    //Route for searching cars
+    Route::post('/search', 'check')->name('car.search');
+});
 
-//Route for creating users
-Route::get('/user', [UsuarioController::class, 'showListUsers'])->name('showListUsers');
-Route::post('/user', [UsuarioController::class, 'add'])->name('user.add');
-Route::delete('/user/{id}', [UsuarioController::class, 'delete'])->name('user.delete');
-Route::put('/user/{id}/edit', [UsuarioController::class, 'edit'])->name('user.edit');
-Route::get('/user/{id}', [UsuarioController::class, 'showEdit'])->name('edituser');
 
-//Route to asign user to car
-Route::get('/asign', [UsuarioController::class, 'show'])->name('asigncar');
-Route::post('/asign', [UsuarioController::class, 'asign'])->name('asigncartouser');
+Route::controller(UsuarioController::class)->group(function(){
+    //Route for creating users
+    Route::get('/user', 'showListUsers')->name('showListUsers');
+    Route::post('/user', 'add')->name('user.add');
+    Route::delete('/user/{id}', 'delete')->name('user.delete');
+    Route::put('/user/{id}/edit', 'edit')->name('user.edit');
+    Route::get('/user/{id}', 'showEdit')->name('edituser');
+
+    //Route to asign user to car
+    Route::get('/asign', 'show')->name('asigncar');
+    Route::post('/asign', 'asign')->name('asigncartouser');
+});
